7286e177c9573eaf9456acd578da7f39894d674b
suvarnaks1 <suvarnaks2002@gmail.com>
2025-04-24 11:22:08 +0530

    ful

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SideMenuPage(),
+      
       routes: {
         '/mobileLogin': (context) => MobileLoginPage(),
         '/tabletLogin': (context) => TabletLoginPage(),
